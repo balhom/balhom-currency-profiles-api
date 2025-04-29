18dba441a2378613291949a04f5e4593f1bfbcd2
fabbo-repo <fscherle98@gmail.com>
2025-04-29 16:29:49 +0200

    Event serialization fix bugfix

--- src/main/kotlin/org/balhom/currencyprofilesapi/modules/currencyprofiles/domain/models/CurrencyProfileSharedUser.kt
+++ src/main/kotlin/org/balhom/currencyprofilesapi/modules/currencyprofiles/domain/models/CurrencyProfileSharedUser.kt
@@ -1,7 +1,9 @@
 package org.balhom.currencyprofilesapi.modules.currencyprofiles.domain.models
 
+import io.quarkus.runtime.annotations.RegisterForReflection
 import java.util.UUID
 
+@RegisterForReflection
 data class CurrencyProfileSharedUser(
     var id: UUID,
     var email: String,
